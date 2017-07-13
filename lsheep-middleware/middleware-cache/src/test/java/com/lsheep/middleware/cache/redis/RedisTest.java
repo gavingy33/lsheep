f64137db7d40ce27b674320e5c29db355698b5cf
GaoFeng <gavingy33@hotmail.com>
2017-07-13 15:09:59 +0800

    no message

---
+++ lsheep-middleware/middleware-cache/src/test/java/com/lsheep/middleware/cache/redis/RedisTest.java
@@ -0,0 +1,24 @@
+package com.lsheep.middleware.cache.redis;
+
+import org.junit.Test;
+
+import redis.clients.jedis.Jedis;
+
+public class RedisTest {
+	
+	@Test
+	public void connect(){
+		Jedis jedis = new Jedis();
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+
+}
