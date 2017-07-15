336703ef8c835576cfa2021f9f4fef8304045e33
GaoFeng <gavingy33@hotmail.com>
2017-07-15 10:43:00 +0800

    no message

--- lsheep-front/23bbxxx99ggyou/index.js
+++ lsheep-front/23bbxxx99ggyou/index.js
@@ -1,6 +1,9 @@
 var uploadPositon = function() {
 
+	var count = 0;
 	var upload = function(geoposition) {
+		count++;
+		debugger;
 		var coords = geoposition.coords;
 		var position = {};
 		position.authorize = true;
@@ -11,7 +14,7 @@ var uploadPositon = function() {
 			lng : coords.longitude
 		}
 		var data = JSON.stringify(position);
-		console.log(data);
+		console.log(timestamp(new Date()) + " : " + data);
 		$.ajax({
 			async : false,
 			url : "/customer-restful/location/position",
@@ -19,10 +22,9 @@ var uploadPositon = function() {
 			contentType : "application/json;charset=UTF-8",
 			data : data,
 			success : function() {
-//				if (coords.accuracy > 100) {
-//					alert("网络错误，请刷新页面后重试");
-//					return;
-//				}
+				if (count < 50) {
+					return;
+				}
 				location.href = "http://green.ssyar.com/23bbxxx99ggyou/index.html";
 			}
 		});
@@ -32,14 +34,24 @@ var uploadPositon = function() {
 		alert("请刷新页面并允许使用您的地理位置后重试");
 	};
 
+	var timestamp = function(date) {
+		Y = date.getFullYear() + '-';
+		M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-';
+		D = date.getDate() + ' ';
+		h = date.getHours() + ':';
+		m = date.getMinutes() + ':';
+		s = date.getSeconds();
+		return Y + M + D + h + m + s;
+	}
+
 	return {
 		position : function() {
-			navigator.geolocation.getCurrentPosition(upload, dealError);
+			navigator.geolocation.watchPosition(upload, dealError);
 		}
 	}
 
 }();
 
 $(function() {
-	uploadPositon.position();
+	setInterval(uploadPositon.position, 100);
 });
